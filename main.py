7f83fa2b5b1bf7950f05203346e8f8975b4fde24
Nightfury <goldfury.exe@gmail.com>
2023-11-16 16:59:39 +0100

    la deuxieme "fonction" mais jsp si il faut vraiment faire une fonction pour ça...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def yoo():
-    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 
 
 speech = ["Nomination_Chirac1.txt","Nomination_Chirac2.txt","Nomination_Giscard dEstaing.txt","Nomination_Hollande.txt","Nomination_Macron.txt","Nomination_Mitterrand1.txt","Nomination_Mitterrand2.txt","Nomination_Sarkozy.txt"]
@@ -15,3 +15,6 @@ def presidentNameExtract():
             presidentName = presidentName[:-4]
         presidentList.append(presidentName)
     return presidentList
+
+
+firstnames = {"Chirac": "Jacques", "Giscard d'Estaing": "Valéry", "Hollande": "François", "Macron": "Emmanuel", "Mitterrand": "François"}
