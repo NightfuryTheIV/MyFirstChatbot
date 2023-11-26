d7416622bc715cac2cbf727185ceb437f96bf14d
dabdab59 <124675225+dabdab59@users.noreply.github.com>
2023-11-26 18:56:11 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,1 +1,8 @@
 
+def extract_name_president(name):
+    president_name = name[11:]
+    if ("1" in president_name) or ("2" in president_name):
+        president_last_name = president_name[:-5]
+    else:
+        president_last_name = president_name[:-4]
+    return president_last_name
