cb485aa23b6f7257b58e3167c57b9df6d00851ea
Nightfury <goldfury.exe@gmail.com>
2023-11-26 22:17:10 +0100

    FIN

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ from fonctions import *
 from features import *
 speech = os.listdir("speeches")
 
-clean_adding()
-TF_IDF("cleaned")
+presidents = presidentNameExtract(speech)
+# The dictionary associating the last names with first names is in fonctions.py, line 30.
 
+display_full_names()
 
-"""
-#First person to speak of écology or climat
-eco(directory)
+clean_adding()  # 4th and 5th points at once
 
-#Words said by all président
-print("They are",All_said(matrice_result))
-"""
+# TF, IDF, and TF-IDF functions are in fonctions.py, from line 65 to line 147
 
+# choice to test one of the 6 features individually :
 choix1 = int(input("HELLO USER! And welcome to our brand new Work In Progress on data pre-processing! We have all kinds of features that will soon be listed in README.md, but however right now you can do one of 6 things:\n1 display the last important words in all of the speeches combined,\n2 Display the words with the highest chances of being important,\n3 Show the most repeated word by Jacques Chirac, \n4 Show which presidents talked about 'Nation' in their speeches, \n5 Show the first president to care about climate change and ecology [WIP]\n6 Display all the meaningful words that have been employed by all the presidents [WIP]"))
 
 if choix1 == 1:
@@ -30,4 +28,4 @@ elif choix1 == 5:
 elif choix1 == 6:
     print(words_said_by_all_presidents(TF("speeches")))
 else:
-    print("Exiting")
+    print("Exiting.")
