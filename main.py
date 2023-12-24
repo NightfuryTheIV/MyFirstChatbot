4ff5503397de0fd3f72ce26c5ddc1802bd774c44
Nightfury <goldfury.exe@gmail.com>
2023-12-24 23:05:52 +0100

    fml

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ presidents = presidentNameExtract(speech)
 clean_files("speeches")
 
 # TF, IDF, and TF-IDF functions are in fonctions.py, from line 65 to line 147
-
+"""
 # choice to test one of the 6 features individually :
 choix1 = int(input("HELLO USER! And welcome to our brand new Work In Progress on data pre-processing! We have all kinds of features that will soon be listed in README.md, but however right now you can do one of 6 things:\n1 display the last important words in all of the speeches combined,\n2 Display the words with the highest chances of being important,\n3 Show the most repeated word by Jacques Chirac, \n4 Show which presidents talked about 'Nation' in their speeches, \n5 Show the first president to care about climate change and ecology [WIP]\n6 Display all the meaningful words that have been employed by all the presidents [WIP]"))
 
@@ -28,5 +28,4 @@ else:
 
 
 TFIDF("cleaned")
-
-
+"""
